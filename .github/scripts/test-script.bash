fcd167a98113eb3d3a84e1b28b9f7d812b1c8449
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2021-10-02 18:16:16 +0200

    github actions: test script

--- .github/scripts/test-script.bash
+++ .github/scripts/test-script.bash
@@ -2,5 +2,18 @@
 perl --version
 
 cd /tmp
-pwd
-printenv
+wget $LABONE_VERSION
+tar -xf LabOne*
+mkdir -p ~/local/lib ~/local/include
+cp LabOne*/API/C/include/ziAPI.h ~/local/include
+cp LabOne*/API/C/lib/libziAPI-linux64.so ~/local/lib
+export LD_LIBRARY_PATH=$HOME/local/lib
+export LIBRARY_PATH=$LD_LIBRARY_PATH
+export CPATH=$HOME/local/include
+printenv LD_LIBRARY_PATH
+
+cd $GITHUB_WORKSPACE
+perl Makefile.PL
+make
+/tmp/LabOne*/DataServer/ziDataServer &
+prove -bv t xt
